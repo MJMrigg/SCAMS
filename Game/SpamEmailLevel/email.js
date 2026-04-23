6c46b1adff961341c89397c4ff26df7c47d31114
MegR9 <megru2004@gmail.com>
2026-04-23 14:59:36 -0400

    Disabled answer toggle after submit
    Disabled answer selections after user submits answers for Vishing and Phishing levels.

--- Game/SpamEmailLevel/email.js
+++ Game/SpamEmailLevel/email.js
@@ -1,10 +1,11 @@
 class email{
-    constructor(img, ans){
+    constructor(img, ans, num){
         //this.imgUrl = '';
         this.imgUrl = img;
         this.done = false;
         //this.corAns = [null, null, null, false, false, null];
         this.corAns = ans;
+        this.count = num;
     }
     checkCorrect(index, value){
 		if(value == this.corAns[index]){
